e000790f9e159ee508ae5ad606511d6eb0557ad4
James Harrison <jamesebharrison@hotmail.com>
2021-10-12 10:41:36 +0100

    removed x infront of describe getGrade test

--- src/js2-functions-intro/js2.test.js
+++ src/js2-functions-intro/js2.test.js
@@ -101,7 +101,7 @@ xdescribe("calculateLifeTimeSupply() tests", () => {
   });
 });
 
-xdescribe("getGrade() tests", () => {
+describe("getGrade() tests", () => {
   it("Should have a return", () => {
     expect(getGrade(70)).toBeDefined();
   });
