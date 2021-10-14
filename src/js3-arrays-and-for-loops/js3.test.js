c5f195c56e3caff44d3f2cce3c01426a677230f4
James Harrison <jamesebharrison@hotmail.com>
2021-10-14 12:29:48 +0100

    removed x from describe

--- src/js3-arrays-and-for-loops/js3.test.js
+++ src/js3-arrays-and-for-loops/js3.test.js
@@ -102,7 +102,7 @@ xdescribe("Testing totalRange()", () => {
   });
 });
 
-xdescribe("Testing moveFirstAndLastItems()", () => {
+describe("Testing moveFirstAndLastItems()", () => {
   it("Should be defined", () => {
     expect(moveFirstAndLastItems(items)).toBeDefined();
   });
